cf4d6aa6737a7cfc35060ad1eeee389ceee110fe
tony24123 <tjdduswns01@naver.com>
2024-10-28 11:49:21 +0900

    데이터 타입 ``중요

---
+++ 1-3-데이터타입/2-string-type.js
@@ -0,0 +1,29 @@
+
+let myName = "김철수";
+console.log(myName);
+
+myName ='박영희';
+console.log(myName);
+
+myName = `둘리`;
+console.log(typeof myName);
+
+// let htmlTag = '<ul>\n\t<li>사과</li>\n\t<li>바나나</li>\n</ul>';
+let htmlTag = `
+  <ul>
+  <li>사과</li>
+  <li>바나나</li>
+  </ul>
+`;
+
+console.log(htmlTag);
+
+// x월 x일은 xxx날입니다.
+
+let month = 12;
+let day = 25;
+let anniversary ='크리스마스';
+
+let sentence = `${month}월 ${day}일은 ${anniversary}입니다.`;
+console.log(sentence);
+
