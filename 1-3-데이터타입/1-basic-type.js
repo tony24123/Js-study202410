ed95f266728af9154c16df31d01e8c3e535556fc
tony24123 <tjdduswns01@naver.com>
2024-10-28 11:24:26 +0900

    js-1-3-데이터타입 기본 학습

---
+++ 1-3-데이터타입/1-basic-type.js
@@ -0,0 +1,11 @@
+let age = 20;
+console.log(typeof age);
+
+// 자바스크립트는 숫자를 전부 실수로 판단
+let taxRate = 0.15;
+console.log(typeof taxRate);
+
+console.log(age / 0); //Infinity 에러는 안남
+console.log(age * '메롱'); //Not a Number
+
+
